caac4992fbe758fd991b7e3495477a8d94192ca7
Roman Matanin <romashka951@gmail.com>
2023-01-11 16:15:23 +0300

    Сохранение ссылок на HTML - элементы, 1048

--- Advanced/wwwroot/interop.js
+++ Advanced/wwwroot/interop.js
@@ -8,3 +8,13 @@
         row.append(cell);
     }
 }
+
+function addTableRows(colCount, elem) {
+    let row = document.createElement("tr");
+    elem.parentNode.insertBefore(row, elem);
+    for (let i = 0; i < colCount; i++) {
+        let cell = document.createElement("td");
+        cell.innerText = "New Elements"
+        row.append(cell);
+    }
+}
