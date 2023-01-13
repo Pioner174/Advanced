91860e56df83ff36fb6e20d7d63101027665b14b
Roman Matanin <romashka951@gmail.com>
2023-01-13 13:45:21 +0300

    Проверка достоверности данные формы и отображение ошибок, 1067

--- Advanced/Models/Person.cs
+++ Advanced/Models/Person.cs
@@ -1,14 +1,23 @@
-﻿namespace Advanced.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Advanced.Models
 {
     public class Person
     {
         public long PersonId { get; set; }
 
+        [Required(ErrorMessage = "Имя - обязательное поле")]
+        [MinLength(3, ErrorMessage ="Имя, должно быть длиннее 3 символов")]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Фамилия - обязательное поле")]
+        [MinLength(3, ErrorMessage = "Фамилия, должно быть длиннее 3 символов")]
         public string Surname { get; set; }
 
+        [Range(1, long.MaxValue, ErrorMessage = "A department must be selected")]
         public long DepartmentId { get; set; }
+
+        [Range(1, long.MaxValue, ErrorMessage = "A location must be selected")]
 
         public long LocationId { get; set; }
 
